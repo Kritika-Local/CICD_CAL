b041a5c5e1da9326b70e11df0bcc05626a507d8f
kritika <kritika.vohra@realthingks.com>
2026-06-25 12:14:54 +0530

    corrected test_add function

--- tests/test_cal.py
+++ tests/test_cal.py
@@ -1,8 +1,8 @@
 from src.cal_operations import add,sub
 
-def test_add(a,b):
+def test_add():
     assert add(2,2)==4
     assert add(3,5)==8
 
-def test_sub(a,b):
+def test_sub():
     assert sub(3,3)==0
